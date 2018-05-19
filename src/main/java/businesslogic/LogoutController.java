ceca2283d4db51b9496d0a58e025f9f489539c94
bertukan <bertukan2017@gmail.com>
2018-05-19 11:00:51 -0500

    user interface

---
+++ src/main/java/businesslogic/LogoutController.java
@@ -0,0 +1,22 @@
+package businesslogic;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class LogoutController extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+        request.getSession().invalidate();
+        dispatcher.forward(request, response);
+        }
+}
