a77424d159dd9579d96ea41f2ec7516d9b67577b
Salahadin <smahmud@mum.edu>
2018-05-19 19:10:30 -0500

    user interface

--- web/js/project.js
+++ web/js/project.js
@@ -1,10 +1,25 @@
 $(function () {
-
         $("#project1").click(function () {
             $.get("/ProjectController")
                 .done(ajaxSuccess)
                 .fail(ajaxFailure);
         });
+
+        $("#createNewProject").click(function () {
+            $("#createNewProjectModal").showModal();
+        });
+
+        $("#txtProjectName").onkeyup(function () {
+            var txtProjName = $("#txtProjectName");
+            if($(this).val() != '' ){
+                $('.enableOnInput').prop('disabled', true);
+                alert(txtProjName.value);
+            }else{
+                $('.enableOnInput').prop('disabled', false);
+                alert("Project name cant be blank");
+            }
+        });
+
 });
 
 function ajaxSuccess(data) {
